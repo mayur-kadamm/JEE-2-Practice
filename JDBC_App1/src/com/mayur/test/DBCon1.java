42e755d4ace26b3b97a2c55629619e3ba230bf37
mayur08kadam <mayur31kadam@gmail.com>
2025-12-06 17:03:21 +0530

    added

---
+++ JDBC_App1/src/com/mayur/test/DBCon1.java
@@ -0,0 +1,26 @@
+package com.mayur.test;
+
+import java.sql.*;
+public class DBCon1 {
+
+	public static void main(String[] args) {
+		try {
+			Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl","system","tiger");
+			Statement stm = con.createStatement();
+			ResultSet rs = stm.executeQuery("select * from product01");
+			
+			while(rs.next()) {
+				System.out.println(rs.getString(1)+"\t"+
+						                         rs.getString(2)+"\t"+
+												 rs.getFloat(3)+"\t"+
+						                         rs.getInt(4)
+						                          );
+			} // end of while 
+			con.close();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
